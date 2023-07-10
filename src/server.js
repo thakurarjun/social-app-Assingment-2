598ac19f860caf8a073b719b5ad0c8f82cfeaf6a
Arjun <thakurarjun880@gmail.com>
2023-07-10 16:14:31 +0530

    chages

--- src/server.js
+++ src/server.js
@@ -53,7 +53,7 @@ export function makeServer({ environment = "development" } = {}) {
     },
 
     routes() {
-      this.namespace = "/api";
+      this.namespace = "api";
       // auth routes (public)
       this.post("/auth/signup", signupHandler.bind(this));
       this.post("/auth/login", loginHandler.bind(this));
