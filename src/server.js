93592a06b4d74b19b77884fd954c1ac5561b2cd7
Arjun <thakurarjun880@gmail.com>
2023-07-10 16:01:23 +0530

    minor change

--- src/server.js
+++ src/server.js
@@ -53,8 +53,7 @@ export function makeServer({ environment = "development" } = {}) {
     },
 
     routes() {
-      // this.namespace = "api";
-      this.urlPrefix = 'http://localhost:3000/'
+      this.namespace = "api";
       // auth routes (public)
       this.post("/auth/signup", signupHandler.bind(this));
       this.post("/auth/login", loginHandler.bind(this));
